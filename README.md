72252b54be2dc68a6ad990f47bd829862933db23
r4sas <r4sas@i2pmail.org>
2020-02-29 22:06:46 +0000

    add boost 1.64.0
    Signed-off-by: r4sas <r4sas@i2pmail.org>

---
+++ README.md
@@ -0,0 +1,15 @@
+Prebuilt Boost libraries
+====
+
+Prebuilt Boost libraries for Android.
+
+Released so you don't have to spend a day messing around with scripts just in include Boost!
+
+*For list of available Boost versions check out repository [branches list](https://github.com/PurpleI2P/Boost-for-Android-Prebuilt/branches)!*
+
+
+WARNING
+====
+
+* 1.64 built with *gcc* for armeabi armeabi-v7a, x86 using [that](https://rubentorresbonet.wordpress.com/2014/08/22/cross-compiling-boost-1-56-for-android-arm-and-intel-on-windows-with-ndk-r9d-and-gcc-4-8/) manual.
+* built with Android NDK r14b using android-19 platform.
---
+++ boost-1_64_0/Android.mk
@@ -0,0 +1,160 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libboost_atomic
+LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/lib/libboost_atomic.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libboost_chrono
+LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/lib/libboost_chrono.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libboost_context
+LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/lib/libboost_context.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libboost_date_time
+LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/lib/libboost_date_time.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libboost_exception
+LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/lib/libboost_exception.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libboost_filesystem
+LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/lib/libboost_filesystem.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libboost_graph
+LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/lib/libboost_graph.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libboost_iostreams
+LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/lib/libboost_iostreams.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libboost_math_c99
+LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/lib/libboost_math_c99.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libboost_math_c99f
+LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/lib/libboost_math_c99f.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libboost_math_c99l
+LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/lib/libboost_math_c99l.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libboost_math_tr1
+LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/lib/libboost_math_tr1.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libboost_math_tr1f
+LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/lib/libboost_math_tr1f.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libboost_math_tr1l
+LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/lib/libboost_math_tr1l.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libboost_prg_exec_monitor
+LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/lib/libboost_prg_exec_monitor.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libboost_program_options
+LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/lib/libboost_program_options.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libboost_random
+LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/lib/libboost_random.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libboost_regex
+LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/lib/libboost_regex.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libboost_serialization
+LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/lib/libboost_serialization.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libboost_signals
+LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/lib/libboost_signals.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libboost_system
+LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/lib/libboost_system.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libboost_test_exec_monitor
+LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/lib/libboost_test_exec_monitor.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libboost_thread
+LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/lib/libboost_thread.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libboost_timer
+LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/lib/libboost_timer.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libboost_unit_test_framework
+LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/lib/libboost_unit_test_framework.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libboost_wave
+LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/lib/libboost_wave.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libboost_wserialization
+LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/lib/libboost_wserialization.a
+include $(PREBUILT_STATIC_LIBRARY)
